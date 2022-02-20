af225ca4c7167959f1b02b9de0dee36cec470bcc
Rodney <rodneypa@LAPTOP-KQJ5G51E.localdomain>
2022-02-20 16:52:36 -0500

    commit tree 28baedeace3448b5d10d83510db1513863870e33

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -4,6 +4,14 @@ import React from "react";
 
 import Header from "../components/Header";
 
+import React from "react";
+
+function Header(props) {
+  return <h1>hello from the Header!</h1>;
+}
+
+export default Header;
+
 test("displays the text 'hello from the Header!'", () => {
   render(<Header />);
 
